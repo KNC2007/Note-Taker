8febb609c105ed2c414b3a28c5c34c1d9cd1396c
Courtney Kalbach <courtneykalbach@Courtneys-MacBook-Pro.local>
2023-12-15 21:24:36 -0500

    working on routes and updated server js

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,17 @@
+const app = require('express').Router();
+const path = require('path');
+
+
+
+app.get('/notes', (req, res) => {
+   res.sendFile(path.join(__dirname, '/public/notes.html')) 
+});
+
+
+// could be / instead of *?
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+
+module.exports = app
---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const route_html = require('./routes/htmlRoutes');
+const route_api = require('./routes/apiRoutes')
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+app.use(route_html);
+app.use(route_api);
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
+
+
+
+
+
+app.get('/api/notes', (req,res) => {
+or
+app.get('/', (req, res) => {
+
+
+app.post('/api/notes', (req,res) => {
+        or
+app.post('/', (req, res) => {
+
+
+
+
+app.delete('/:note_id', (req, res) => {
+
+});
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for feedback page
+app.get('/feedback', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+);
+
+
+
+// GIVEN a note-taking application
+// WHEN I open the Note Taker
+// THEN I am presented with a landing page with a link to a notes page
+// WHEN I click on the link to the notes page
+// THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
+// WHEN I enter a new note title and the note’s text
+// THEN a "Save Note" button and a "Clear Form" button appear in the navigation at the top of the page
+// WHEN I click on the Save button
+// THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes and the buttons in the navigation disappear
+// WHEN I click on an existing note in the list in the left-hand column
+// THEN that note appears in the right-hand column and a "New Note" button appears in the navigation
+// WHEN I click on the "New Note" button in the navigation at the top of the page
+// THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column and the button disappears
